90589fea9a0dfe3a5a11b0968b976c366991551e
Марк <lolka-pop@rambler.ru>
2023-10-14 21:33:42 +0300

    feat: create header&footer&add react-bootstrap

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export const Footer = () => {
+	const currentYear = new Date().getFullYear();
+	return (
+		<footer>
+			<Container>
+				<Row>
+					<Col className="text-center py-3">
+						<p>ProShop &copy; {currentYear}</p>
+					</Col>
+				</Row>
+			</Container>
+		</footer>
+	);
+};
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import products from '../products.js';
+
+export const Home = () => {
+	return <div>Home</div>;
+};
