027241f7ee31e623e4b4efba295687a14772c543
Марк <lolka-pop@rambler.ru>
2024-01-08 01:43:32 +0300

    fix: change BASE_URL for constant.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000/api';
+export const BASE_URL = '';
 export const PRODUCTS_URL = '/products';
 export const USERS_URL = '/users';
 export const ORDERS_URL = '/orders';
