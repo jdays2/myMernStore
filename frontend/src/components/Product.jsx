1d486c1ca5a658110ac98d8a16e51cb7da3b4634
Марк <lolka-pop@rambler.ru>
2024-01-13 19:33:30 +0300

    feat: change cardImage sizes & others ui fixes

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -4,25 +4,30 @@ import { Link } from 'react-router-dom';
 import { Rating } from './Rating';
 
 const Product = ({ product }) => {
-	
 	return (
 		<Card className="my-3 p-3 flex-grow-1">
-			<Link to={`/product/${product._id}`}>
+			<Link to={`/product/${product._id}`} className='w-100 h-100'>
 				<Card.Img
 					variant="top"
 					src={product.image}
+					className="object-fit-cover border rounded w-100 h-100"
 				/>
 			</Link>
 
 			<Card.Body>
 				<Link to={`/product/${product._id}`}>
-					<Card.Title as="div" className='product-title'>
+					<Card.Title
+						as="div"
+						className="product-title">
 						<strong>{product.name}</strong>
 					</Card.Title>
 				</Link>
 
 				<Card.Text as="div">
-					<Rating value={product.rating} content={`${product.numReviews} reviews`}/>
+					<Rating
+						value={product.rating}
+						content={`${product.numReviews} reviews`}
+					/>
 				</Card.Text>
 				<Card.Text as="h3">${product.price}</Card.Text>
 			</Card.Body>
