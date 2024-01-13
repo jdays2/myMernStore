0601358edd2631730bb7c954a8c3f5343f289001
Марк <lolka-pop@rambler.ru>
2024-01-14 02:48:10 +0300

    test

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,20 @@ app.get('/', (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
+	);
+} else {
+	const __dirname = path.resolve();
+	app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+	app.get('/', (req, res) => {
+		res.send('API is running....');
+	});
+}
+
 app.listen(port, () => {
 	console.log(`Server is running on ${port} port`);
 });
