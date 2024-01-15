a94dbce1a7dc92b18fc7cadf9c479798bc4347ea
Марк <lolka-pop@rambler.ru>
2024-01-15 17:15:11 +0300

    test

--- backend/server.js
+++ backend/server.js
@@ -44,10 +44,6 @@ app.get('/', (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-app.get('/', (req, res) => {
-	res.send('API is running....');
-});
-
 app.listen(port, () => {
 	console.log(`Server is running on ${port} port`);
 });
