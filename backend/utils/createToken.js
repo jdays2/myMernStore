167a343fcd27148a8366313420b8ba335679a798
Марк <lolka-pop@rambler.ru>
2024-01-15 22:18:23 +0300

    test

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,25 +1,21 @@
 import jwt from 'jsonwebtoken';
 
 const createToken = (res, userId) => {
-	console.log('Entering createToken function');
-	
 	try {
 			const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
 					expiresIn: '30d',
 			});
 
-			console.log('Token created successfully:', token);
-
 			// Set JWT as an HTTP-Only cookie
 			res.cookie('jwt', token, {
 					httpOnly: true,
-					secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-					sameSite: 'strict', // Prevent CSRF attacks
-					maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+					secure: process.env.NODE_ENV !== 'development',
+					sameSite: 'strict',
+					maxAge: 30 * 24 * 60 * 60 * 1000,
 			});
 	} catch (error) {
 			console.error('Error creating token:', error);
-			throw error;
+			// Дополнительные действия по логированию ошибки или отправке уведомления
 	}
 };
 
