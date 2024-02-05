093b67b2be52f95ceea42931607ec98edccfef4f
Марк <lolka-pop@rambler.ru>
2024-02-05 23:26:02 +0300

    test

--- frontend/src/redux/slices/productsApiSlice.js
+++ frontend/src/redux/slices/productsApiSlice.js
@@ -22,8 +22,8 @@ export const productsApiSlice = apiSlice.injectEndpoints({
 		getProductDetails: builder.query({
 			query: (id) => ({
 				url: `${PRODUCTS_URL}/${id}`,
-				credentials: 'include',
 			}),
+			credentials: 'include',
 			keepUnusedDataFor: 5,
 		}),
 
